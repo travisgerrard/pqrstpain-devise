50e4522ccd14f0613f2395e471393ea72a49870b
Travis Gerrard <travisgerrard@gmail.com>
2014-02-15 12:41:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PqrstpainDevise::Application.config.secret_key_base = 'cc55a988c066b9da056ff1d01bc760c5c28c59868a24fe08c3cec0beb16aea5f75f3ef987d066e916b65b6e5c293077e6b3dc1550eaf530740629f191e86e251'
